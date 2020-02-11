dfa766e097ae78ab51fea440670c4b27d1f6003c
MrKuBu <mrkubu122333@gmail.com>
2020-02-11 21:48:03 +0300

    *New way*
    Fix old and add new

--- lua/autorun/server/mk_ac_anti_bhop.lua
+++ lua/autorun/server/mk_ac_anti_bhop.lua
@@ -1,5 +1,10 @@
 --Анти Заичьи прижки / Bhop.
 hook.Add("OnPlayerHitGround", "Anti-Bhop", function(player)
 	--Прсмотр прикосновения человека с землёй.
-	player:SetVelocity(- player:GetVelocity() / 2)
+	--player:SetVelocity(- player:GetVelocity() / 2) -- Старый способ(подкивание вверх - не очень актуально)
+	local vel = player:GetVelocity()
+	if 300 == 0 or ( vel.x > 300 or vel.x < -300 or vel.y > 300 or vel.y < -300 ) then
+		local suppressor = 1 + (50 / 100)
+		player:SetVelocity( Vector( -( vel.x / suppressor ), -( vel.y / suppressor ), 0 ) )
+	end
 end)
